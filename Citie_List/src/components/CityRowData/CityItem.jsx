fcb8091545288996686ed1d976c780b22bf0a166
Rupendra <136944198+RupendraCHA@users.noreply.github.com>
2024-09-26 23:36:34 +0530

    Adding Test File for Deploying

--- Citie_List/src/components/CityRowData/CityItem.jsx
+++ Citie_List/src/components/CityRowData/CityItem.jsx
@@ -17,19 +17,27 @@ const CityItem = (props) => {
 
 
   const items = [
+    // {
+    //   key: '1',
+    //   label: (
+    //     <a target="_blank" href="/cityWeather">
+    //       View Weather in new tab
+    //     </a>
+    //   )
+    // },
     {
-      key: '1',
+      key: '2',
       label: (
-        <a target="_blank" href="/cityWeather">
+        <a id='drop-menu-option' target="_blank" href="/cityWeather">
           View Weather in new tab
         </a>
-      ),
+      )
     }
   ]
   return (
     (<tr className='city-row' key={city.id}>
       <td>{index + 1}</td>
-      <Dropdown menu={{ items }} trigger={['contextMenu']}>
+      <Dropdown menu={{ items }} trigger={['contextMenu']} >
         <td onClick={() => handleOnclick(true, city.name)}><Link to="/cityWeather" className='city-name'>
           {city.name}
         </Link></td>
